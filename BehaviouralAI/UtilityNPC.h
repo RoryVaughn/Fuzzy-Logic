0893023d31d74b606b6026ab7c0590f0c697c1e2
Rory Vaughn <Rvaughn2525@gmail.com>
2017-01-09 17:59:53 -0600

    end of day

--- BehaviouralAI/UtilityNPC.h
+++ BehaviouralAI/UtilityNPC.h
@@ -1,6 +1,9 @@
 #ifndef UTILITY_NPC_H_
 #define UTILITY_NPC_H_
 #include "src/BaseNPC.h"
+#include "src/UtilityValue.h"
+#include "src/UtilityScore.h"
+#include <map>
 
 namespace UtilitySystem
 {
@@ -14,6 +17,8 @@ namespace UtilitySystem
 		~UtilityNPC();
 	protected:
 		void selectAction(float a_fdeltaTime) override;
+	private:
+		UtilityValue m_waterValue;		std::map<std::string, UtilityScore*> m_pUtilityScoreMap;
 	};
 }
 #endif
